eea6808f6aa35a07c508045ac800402a588e89dc
RC Pelisco <rcpelisco@gmail.com>
2017-09-26 08:04:38 +0800

    Cleaning

--- The Legend of Butuan - The Masao Warrior/Assets/Scripts/Quest/QuestManager.cs
+++ The Legend of Butuan - The Masao Warrior/Assets/Scripts/Quest/QuestManager.cs
@@ -4,10 +4,50 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour {
 
-	public Quest[] quests;
-	
-	void Awake() {
+	public static QuestManager questManager;
+	List<Quest> quests = new List<Quest>();
+	List<Quest> currentQuest = new List<Quest>();
 
+	void Awake() {
+		if(questManager == null) {
+			questManager = this;
+		} else if(questManager != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void AddQuestItem(string questObjective, int amount) {
+		for(int i = 0; i < currentQuest.Count; i++) {
+			if(currentQuest[i].questObjective == questObjective && currentQuest[i].progress == Quest.QuestProgress.ACCEPT);
+		}
+	}
+
+	public bool RequestAvailableQuest(int questID) {
+		for(int i = 0; i < quests.Count; i++) {
+			if(quests[i].id == questID && quests[i].progress == Quest.QuestProgress.AVAILABLE) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public bool RequestAcceptedQuest(int questID) {
+		for(int i = 0; i < quests.Count; i++) {
+			if(quests[i].id == questID && quests[i].progress == Quest.QuestProgress.ACCEPT) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public bool RequestCompleteQuest(int questID) {
+		for(int i = 0; i < quests.Count; i++) {
+			if(quests[i].id == questID && quests[i].progress == Quest.QuestProgress.COMPLETE) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
