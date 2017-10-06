3d0b2f6ab1c327bdf8cdf08ff1d3ac09068b14ac
RC Pelisco <rcpelisco@gmail.com>
2017-10-07 00:38:10 +0800

    SignPrefab

---
+++ The Legend of Butuan - The Masao Warrior/Assets/Scripts/Character/CharacterLevelModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLevelModel : MonoBehaviour {
+
+	public int currentLevel;
+	private int currentExp;
+	private int[] requiredExp;
+
+	void Awake() {
+		currentLevel = 1;
+	}
+	
+	public int GetCurrentExp() {
+		return currentExp;
+	}
+
+	public int GetRequiredExp() {
+		return requiredExp[currentExp];
+	}
+
+	void Update () {
+
+	}
+}
