2f1133e599289645df694fb76dc7c4f3143bd5a7
RC Pelisco <rcpelisco@gmail.com>
2017-10-13 23:31:06 +0800

    AreaTitle

---
+++ The Legend of Butuan - The Masao Warrior/Assets/AreaTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTitle : MonoBehaviour {
+
+	private Animator anim;
+	private GameObject title;
+
+	void Awake() {
+		anim = GetComponentInChildren<Animator>();
+		title = anim.gameObject;
+		title.SetActive(false);
+		StartCoroutine(Show());
+	}
+
+	IEnumerator Show() {
+		yield return new WaitForSeconds(.5f);
+		title.SetActive(true);
+		StartCoroutine(Hide());
+	}
+
+	IEnumerator Hide() {
+		yield return new WaitForSeconds(2.75f);
+		anim.SetBool("Hide", true);
+	}
+}
